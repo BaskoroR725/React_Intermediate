18f643a92432014467d3cb14ad8d10a4535043e5
Baskoro Ramadhan <baskoror725@gmail.com>
2024-10-08 20:32:45 +0700

    re render state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import './App.css'
 import Counter from './Counter'
 import Dumbo from './Dumbo'
+import ScoreKeeper from './ScoreKeeper'
+import EmojiClicker from './EmojiClicker'
 
 function App() {
   return (
     <>
-      <h1>State Demo</h1>
+    {/*   <h1>State Demo</h1>
       <Counter/>
       <Dumbo />
+      <ScoreKeeper/> */}
+      <EmojiClicker/>
     </>
   )
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 export default function Counter(){
+    console.log('Re-render');
     const [count,setCount] = useState(0);
     const incrementOne = () =>{
         setCount(count + 1);
@@ -10,11 +11,15 @@ export default function Counter(){
         setCount((previousCount) => previousCount + 1);
         setCount((previousCount) => previousCount + 1);
     }
+    const setToTen = () =>{
+        setCount(10);
+    }
     return(
         <div>
             <p>Count: {count}</p>
             <button onClick={incrementOne}>+1</button>
             <button onClick={incrementThree}>+3</button>
+            <button onClick={setToTen}>Set to 10</button>
         </div>
     )
 }
